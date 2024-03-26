b324f527853d3527bde6cee4711bb98e16821352
Anurag Kumar <anuragk.2571@gmail.com>
2024-03-26 10:41:24 +0530

    rest controller

--- demo/src/main/java/com/origin/demo/MyController.java
+++ demo/src/main/java/com/origin/demo/MyController.java
@@ -1,17 +1,18 @@
 package com.origin.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyController {
     @RequestMapping("m")
-   
-    
-    public static String myMethod(){
-        return "Hello";
+   public static String myMethod(){
+        return "<h1>Hello</h1>";
     }
+    @PostMapping("process-form")
+    public static String getData(@RequestParam int num1,@RequestParam int num2) {
+        int res=num1+num2;
+        return "result="+res;
+    }
+    
+   
 }
